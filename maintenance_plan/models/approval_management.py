2300facc0719af9c6f7c34ec8aa007b94a437533
lrc <1164792058@qq.com>
2019-01-08 14:13:14 +0800

    工單審批model優化

--- maintenance_plan/models/approval_management.py
+++ maintenance_plan/models/approval_management.py
@@ -10,14 +10,12 @@ class ApprovalManagement(models.Model):
     _name = 'maintenance_plan.approval'
     _description = '審批管理'
 
-    num = fields.Many2one('maintenance_plan.maintenance.plan', '工單')
-    work_order = fields.Char('工單編號', compute='_get_work_order')
-    # TODO: 提交人
-    submitter = fields.Many2one('user.employees_get', '提交人')
+    maintenance_plan_id = fields.Many2one('maintenance_plan.maintenance.plan', '工單')
+    work_order = fields.Char('工單編號', compute='_get_maintenance_plan_id')
+    submitter = fields.Many2one('res.users', '提交人')
     submit_time = fields.Datetime('提交時間')
     # TODO: 群組
-    # TODO: 審批人
-    approver = fields.Many2one('user.employees_get', '審批人')
+    approver = fields.Many2one('res.users', '審批人')
     status = fields.Selection(STATUS, string='審批狀態')
     approval_time = fields.Datetime('審批時間')
     work_order_description = fields.Char('工單描述:', compute='_get_work_order')
@@ -25,14 +23,15 @@ class ApprovalManagement(models.Model):
     actual_start_time = fields.Char('實際開始時間:', compute='_get_work_order')
     actual_end_time = fields.Char('實際結算時間:', compute='_get_work_order')
 
-    @api.depends('num', 'work_order_description', 'plan_end_time', 'actual_start_time', 'actual_end_time')
-    def _get_work_order(self):
+    @api.depends('maintenance_plan_id')
+    def _get_maintenance_plan_id(self):
         for record in self:
-            record.work_order = record.num.num
-            record.actual_end_time = record.num.actual_end_time
-            record.actual_start_time = record.num.actual_start_time
-            record.plan_end_time = record.num.plan_end_time
-            record.work_order_description = record.num.work_order_description
+            if record.maintenance_plan_id:
+                record.work_order = record.maintenance_plan_id.num
+                record.actual_end_time = record.maintenance_plan_id.actual_end_time
+                record.actual_start_time = record.maintenance_plan_id.actual_start_time
+                record.plan_end_time = record.maintenance_plan_id.plan_end_time
+                record.work_order_description = record.maintenance_plan_id.work_order_description
 
 
 class ReferenceExaminationApproval(models.Model):
