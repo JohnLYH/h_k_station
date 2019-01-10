2b09381215925951e300093a6b0c2c5ebc6fc2e8
lrc <1164792058@qq.com>
2019-01-10 11:31:27 +0800

    no message

--- other_equipment/static/js/freq_of_cal.js
+++ other_equipment/static/js/freq_of_cal.js
@@ -4,62 +4,19 @@ odoo.define('freq_of_cal_btn', function (require) {
     var core = require('web.core');
     var Widget = require('web.Widget');
     var widgetRegistry = require('web.widget_registry');
-
+    console.log('我進來了按鈕')
     var freq_of_cal_btn = Widget.extend({
-        propositions: [],
-        events: {
-            'click .search_extend_reset': 'reset_search',
-            'click .search_extend_apply': 'commit_search',
-            'click .search_extend_export': 'export_search',
-            'keyup .o_searchview_extended_prop_value': function (ev) {
-                if (ev.which === $.ui.keyCode.ENTER) {
-                    this.commit_search();
-                }
-            },
-        },
+        template: 'WidgetWebsiteButton',
 
-        init: function (parent, fields, pannel_template, search_view) {
-            this.fields = fields;
-            this.pannel_template = pannel_template;
-            this.search_view = search_view
+        init: function (parent) {
             this._super(parent, arguments);
         },
 
-        // 读取模板，同时读取属性并构造搜索对象
+        // 替換模板
         renderElement: function () {
-            var $el;
-            var bHaveExt = false
-            if (this.pannel_template) {
-                this.propositions = [];
-                $el = $(core.qweb.render(this.pannel_template, {
-                    widget: this
-                }).trim());
-                var fields_place_holders = $el.find("[for]")
-                for (var i = 0; i < fields_place_holders.length; i++) {
-                    var holder = fields_place_holders[i]
-                    var filed_name = $(holder).attr('for')
-                    var option = $(holder).attr('option') || '{"range": true}'
-                    option = pyeval.py_eval(option)
-                    var field = this.fields[filed_name]
-                    if (field) {
-                        var prop = new layui_search_proposition(this, field, option);
-                        prop.appendTo(holder)
-                        this.propositions.push(prop);
-                        bHaveExt = true
-                    }
-                }
-            } else {
-                $el = this._make_descriptive();
-            }
-
+            var $el = '<input name="Fruit" type="radio" value="" />苹果 </label>';
             this.replaceElement($el);
-            if (bHaveExt) {
-                this.do_show();
-            } else {
-                this.do_hide();
-            }
         },
-
     });
 
     widgetRegistry.add("freq_of_cal_btn", freq_of_cal_btn);
