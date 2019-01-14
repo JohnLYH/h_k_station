06a2fe32bf3c6d290bbe08c061c72c72d0ee0f5c
Artorias <305111632@qq.com>
2019-01-14 10:57:55 +0800

    css

--- maintenance_plan/static/js/equipment/equipment_tree_button.js
+++ maintenance_plan/static/js/equipment/equipment_tree_button.js
@@ -3,20 +3,18 @@ odoo.define('equipment_tree_button', function (require) {
 
     var widget_registry = require('web.widget_registry');
     var tree_button = require('treebtns');
+    var data_manager = require('web.data_manager');
 
     var equipment_tree_button = tree_button.extend({
         template: 'tem_equipment_tree_button',
         _click_tree_buttons: function (event) {
             var self = this;
             event.stopPropagation();
-            self._rpc({
-                model: 'maintenance_plan.maintenance.plan',
-                method: 'get_ref_id',
-                args: ['maintenance_plan.act_equipment_management']
-            }).then(function (act_id) {
-                self.do_action(act_id, {
+            data_manager.load_action('maintenance_plan.act_equipment_management').then(function (result) {
+                self.do_action(result, {
                     res_id: self.id,
                     view_type: 'form',
+                    replace_last_action: true
                 })
             })
         }
